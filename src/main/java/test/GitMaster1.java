28a4147fe2c01d72c545a227e9b80b8709855bec
daryal89 <creativity80@gmail.com>
2018-11-30 09:31:23 -0500

    pushing the new files and completeJava file that includes java concents

---
+++ src/main/java/test/GitMaster1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class GitMaster1 {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "how ";
+		String s3 = "are";
+		String s4 = "you";
+		System.out.println("String Concatination " + s1 + s2 + s3 + s4);
+
+	}
+
+}
